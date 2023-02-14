834ba1c7c6af6c2c14229893bbcba82436c487e8
ThiagoZambelli <thiagozamoliveira@gmail.com>
2023-02-14 18:52:56 -0300

    Criação da validação do primeiro digito do CPF, extremamente interessante. Ja funcional

--- app/validaCPF.js
+++ app/validaCPF.js
@@ -1,6 +1,7 @@
 export default function ehUmCPF (campo){
     const cpf = campo.value.replace(/\.|-/g, "");
     validaNumerosRepitidos(cpf);
+    validPrimeiroDigito(cpf);
 }
 
 
@@ -18,4 +19,27 @@ function validaNumerosRepitidos(cpg){
         '99999999999'
     ]
     return numeroRepetido.includes(cpf);
+}
+
+//A validação do primeiro digito funciona da seguinte forma: o primeiro disgito é 
+//a soma de todos os 9 numeros do cpf sendo multiplicados em ordem decrescente do 10 ao 2
+// dpois o resultado por 10 e pegando o modulo deste resultado sendo dividido por 11.
+function validPrimeiroDigito(cpf){
+
+    let soma = 0;
+    let multiplicador = 10;
+
+    for(let tamanho = 0; tamanho < 9; tamanho++){
+        soma += cpf[tamanho] * multiplicador;
+        multiplicador--;
+    }
+
+    soma = (soma * 10) % 11;
+
+    if(soma == 10 || soma == 1){
+        soma = 0;
+    }
+    
+    return soma != cpf[9];
+    
 }
